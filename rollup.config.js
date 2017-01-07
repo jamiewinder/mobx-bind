cbdca5e6f5ae1d58406d0486ae489c7f722cb9c3
jamiewinder <jamiewinder@gmail.com>
2017-01-07 15:37:54 +0000

    npm config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 export default {
     entry: 'lib/index.js',
-    dest: 'dist/mobx-bind.js',
+    dest: 'dist/mobx-bind.umd.js',
     format: 'umd',
     moduleName: 'mobxBind',
     external: ['mobx']
