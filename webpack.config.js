748cd8a459528bf96dee0fc48f768dff81e0a28b
jamiewinder <jamiewinder@gmail.com>
2017-01-11 14:32:36 +0000

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+var webpack = require('webpack');
 module.exports = {
     entry: __dirname + '/lib/index.js',
     output: {
+        libraryTarget: 'umd',
+        library: 'mobxBind',
         path: __dirname + '/dist',
         filename: 'mobx-bind.umd.js',
-        libraryTarget: 'umd'
     },
-    externals: [
-        'mobx'
+    resolve: {
+        modules: [
+            __dirname + 'node_modules'
+        ],
+        extensions: ['.js']
+    },
+    externals: {
+        'mobx': 'mobx'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
     ]
 };
